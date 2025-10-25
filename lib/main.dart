35c8776310399759ceb5fa8992721ddc3164c431
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-25 20:58:27 +0200

    add styling and padding plus custom fonts to list view

--- lib/main.dart
+++ lib/main.dart
@@ -48,11 +48,24 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget buildRecipeCard(Recipe recipe) => Card(
-    child: Column(
-      children: <Widget>[
-        Image(image: AssetImage(recipe.imageUrl, bundle: null)),
-        Text(recipe.label),
-      ],
+    elevation: 2.0,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          const SizedBox(height: 14.0),
+          Text(
+            recipe.label,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Palatino',
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
